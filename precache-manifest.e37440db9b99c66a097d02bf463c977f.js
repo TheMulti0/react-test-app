fe3951fbc3289f6e7ab30b19ae8968a74c7fd1d5
TheMulti0 <multi@codeprecise.com>
2020-03-30 09:11:24 +0300

    Updates

---
+++ precache-manifest.e37440db9b99c66a097d02bf463c977f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "619e5f511d0449828e534b4fd306ed59",
+    "url": "/react-test-app/index.html"
+  },
+  {
+    "revision": "7822f4b128cb5f070a9c",
+    "url": "/react-test-app/static/css/main.230a0d0f.chunk.css"
+  },
+  {
+    "revision": "2cbde1fc472667a1dfc1",
+    "url": "/react-test-app/static/js/2.276f1275.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/react-test-app/static/js/2.276f1275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7822f4b128cb5f070a9c",
+    "url": "/react-test-app/static/js/main.c0481a4f.chunk.js"
+  },
+  {
+    "revision": "fadd6952376beb84c0d9",
+    "url": "/react-test-app/static/js/runtime-main.cdb1e591.js"
+  }
+]);
